bff0f24019d2ef51a22c6c0e0e40b02126ead3e6
AryanDhane <dhanearyan150074@gmail.com>
2026-05-10 17:27:57 +0530

    add pattern file

---
+++ Pattern/ex2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+int n = 5;
+
+for (int i= 0; i<n; i++){ // outer loop
+    char ch =  'A';
+    for (int j=0; j<n; j++){ // inner loop
+        cout << ch<< " ";
+        ch++;
+    }
+    cout << endl;
+}
+
+    return 0;
+}
