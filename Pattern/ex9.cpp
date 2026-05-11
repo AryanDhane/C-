1cea451bce560169eff3bb8f26410f7dce8cc272
AryanDhane <dhanearyan150074@gmail.com>
2026-05-11 13:22:25 +0530

    add new file

---
+++ Pattern/ex9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n = 9;
+for (int i=0; i<n; i++){
+    for(int j=0; j<n-i-1; j++){
+        cout << "  ";
+    }
+    for (int j=1; j<=i+1; j++){
+        cout << j << " ";
+    }
+    for (int j=i; j>0; j--){
+        cout << j << " ";
+    }
+    cout << endl;
+}
+
+    return 0;
+}
+
+
+// Output:
+//                 1    
+//               1 2 1
+//             1 2 3 2 1
+//           1 2 3 4 3 2 1
+//         1 2 3 4 5 4 3 2 1
+//       1 2 3 4 5 6 5 4 3 2 1
+//     1 2 3 4 5 6 7 6 5 4 3 2 1
+//   1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+// 1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1        
