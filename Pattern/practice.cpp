abbf2a9f8b81a08ef49fe6f010cc9f03f0ab2e3d
AryanDhane <dhanearyan150074@gmail.com>
2026-05-12 08:52:45 +0530

    Add new file

--- Pattern/practice.cpp
+++ Pattern/practice.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main(){
 int n = 4;
 
+char ch = 'A';
+
 for(int i=0; i<n; i++){
-    for(int j=0; j<i+1; j++){
-        cout << " * " << " ";
-       
+    for (int j=0; j<=i; j++){
+        cout << char('A'+i) << " ";
+        ch++;
     }
     cout << endl;
 }
+
     return 0;
 }
