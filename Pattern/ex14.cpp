09f82eb0e2bc64e3f1a7fa52ac2e3ad61e112f50
AryanDhane <dhanearyan150074@gmail.com>
2026-05-14 09:27:32 +0530

    ..

--- Pattern/ex14.cpp
+++ Pattern/ex14.cpp
@@ -8,17 +8,33 @@ int n =4;
 for (int i=0; i<n; i++){
 
     for (int j=0; j<n-i-1; j++){
-        cout << " ";
+        cout << "  ";
    }
-    cout << "*";
+    cout << "*" << " ";
 
     if (i != 0){
      for (int j=0; j<2*i-1; j++){
-        cout << " ";
+        cout << "  ";
      }
-     cout << "*";    
+     cout << "*" << " ";    
 }
 cout << endl;
 }
-    return 0;
+
+// bottom part
+
+for (int i=0; i<n-1; i++){
+    for (int j=0; j<=i; j++){
+        cout << "  ";
+    }
+    cout << "*" << " ";
+    if (i != n-2){
+        for (int j=0; j<2*(n-i-2)-1; j++){
+            cout << "  ";
+        }
+        cout << "*" << " ";
+    }
+    cout << endl;
+}
+return 0;
 }
