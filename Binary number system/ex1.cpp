fbe5d966f858b7563f059469dcbefcd59138f8dc
AryanDhane <dhanearyan150074@gmail.com>
2026-05-14 12:00:26 +0530

    ..

---
+++ Binary number system/ex1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter a decimal number: ";
+    cin >> n;
+
+    int binary[32];   // array to store binary digits
+    int i = 0;
+
+    // conversion
+    while(n > 0) {
+        binary[i] = n % 2;   // store remainder (0 or 1)
+        n = n / 2;           // reduce number
+        i++;
+    }
+
+    // print in reverse order
+    cout << "Binary: ";
+    for(int j = i - 1; j >= 0; j--) {
+        cout << binary[j];
+    }
+
+    return 0;
+}
--- Pattern/ex15.cpp
+++ Pattern/ex15.cpp
@@ -1,35 +1,32 @@
 // Butterfiy pattern
 
+
 #include <iostream>
-using namespace std;    
+using namespace std;
 
-int main (){
-int n = 4;
+int main() {
 
-for (int i=0; i<n; i++){ // outer loop
-    for (int j=0; j<i+1; j++){ // inner loop
-        cout << "*" <<" ";
-    }
-    for (int j=0; j<2*(n-i-1); j++){ // inner loop
-        cout << " " <<" ";
-    }
-    for (int j=0; j<i+1; j++){
-       cout << "*" <<" ";
+    int n = 4;
+
+    for(int i = 1; i <=n; i++) {
+
+        // Left stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
         }
-         // inner loop
-        cout << "*" <<" ";
-    }
-    cout << endl;
 
-for (int i=0; i<n-1; i++){ // outer loop
-    for (int j=0; j<n-i-1; j++){ // inner loop
-        cout << "*" <<" ";
-    }
-    
+        // Middle spaces
+        for(int j = 1; j <= 2*(n - i); j++) {
+            cout << " ";
+        }
+
+        // Right stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
         cout << endl;
     }
 
-
-
-return 0;
+    return 0;
 }
