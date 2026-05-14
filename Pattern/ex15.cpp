6889ccdde533a025c786fd0f2c6303e7faa42a76
AryanDhane <dhanearyan150074@gmail.com>
2026-05-14 10:55:50 +0530

    ..

--- Pattern/ex15.cpp
+++ Pattern/ex15.cpp
@@ -13,12 +13,23 @@ for (int i=0; i<n; i++){ // outer loop
     for (int j=0; j<2*(n-i-1); j++){ // inner loop
         cout << " " <<" ";
     }
-    for (int j=0; j<i+1; j++){ // inner loop
+    for (int j=0; j<i+1; j++){
+       cout << "*" <<" ";
+        }
+         // inner loop
         cout << "*" <<" ";
     }
     cout << endl;
-}
-for (int )
+
+for (int i=0; i<n-1; i++){ // outer loop
+    for (int j=0; j<n-i-1; j++){ // inner loop
+        cout << "*" <<" ";
+    }
+    
+        cout << endl;
+    }
+
+
 
 return 0;
 }
