2b7fc6fae552c761f579242d30421fc2d64e9c71
AryanDhane <dhanearyan150074@gmail.com>
2026-05-20 09:52:06 +0530

    ..

---
+++ Array/ex4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void changeArr(int arr[], int size){
+    for (int i=0; i<size; i++){
+        arr[i] = arr[i] * 2;
+    }
+}
+
+int main() {
+    int arr[5] = {1,2,3,4,5};
+    int size = 5;
+
+    changeArr(arr, size);
+    for (int i=0; i<size; i++){
+        cout << (arr[i]) << endl;
+    }
+
+    cout << endl;
+    return 0;
+}
