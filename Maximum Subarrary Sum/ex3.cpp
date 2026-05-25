764403e63ac99da2515e2ea098b7998077b6725d
AryanDhane <dhanearyan150074@gmail.com>
2026-05-25 13:20:47 +0530

    .

---
+++ Maximum Subarrary Sum/ex3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main() {
+
+    int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+
+    int n = 9;
+
+    int currentSum = 0;
+    int maxSum = INT_MIN;
+
+    for(int i = 0; i < n; i++) {
+
+        currentSum += arr[i];
+
+        maxSum = max(maxSum, currentSum);
+
+        if(currentSum < 0) {
+            currentSum = 0;
+        }
+    }
+
+    cout << "Maximum Subarray Sum = " << maxSum;
+
+    return 0;
+}
