a07fc65beb404d34280f584698885e3dde82f914
AryanDhane <dhanearyan150074@gmail.com>
2026-05-25 10:59:13 +0530

    .

--- Maximum Subarrary Sum/ex1.cpp
+++ Maximum Subarrary Sum/ex1.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 int main(){
 
+    int arr[] = {1,2,3,4,5,6,7};
 
-    
+    int n = 7;
 
+    for (int st=0; st<n; st++){
+        for (int en=st; en<n; en++){
+            for (int i=st; i<=en; i++){
+                cout << arr[i] ;
+            }
+            cout << endl;
+        }
+    }
     return 0;
 
 }
